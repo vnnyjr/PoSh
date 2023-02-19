8c12c58a60e55a1a4c23f66aeb87578c64954b48
vnnyjr <73672692+vnnyjr@users.noreply.github.com>
2023-02-19 11:10:26 -0500

    small edit proof of concept

--- function_Get-TimeUntilBirthday.ps1
+++ function_Get-TimeUntilBirthday.ps1
@@ -7,6 +7,5 @@ function Get-TimeUntilBirthday {
 
 }
 
-# making a change in code, commit and push to the branch, pull request into the main/master
-
-Get-TimeUntilBirthday 06/21
+write-host "Time until Jesus B-Day" -foregroundcolor yellow
+Get-TimeUntilBirthday 12/25
